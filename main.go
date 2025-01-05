9e6ea7f04871a2885615707530feeaa1a05d1f74
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-01-04 18:59:07 -0700

    Learn data types in Go lang

--- main.go
+++ main.go
@@ -8,8 +8,18 @@ func main() {
 	var conferenceName = "Go conference"
 	const conferenceTickets = 50
 	var remainingTickets = 50
+
+	fmt.Printf("conferenceTickets is %T, remainingTicekts is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+	//ask user for their name
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 
 }
