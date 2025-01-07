9505df04319589d379aa66f85a9674f7d4d08332
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-01-06 18:10:02 -0700

    Test if/else conditional and learn boolean data type

--- main.go
+++ main.go
@@ -45,5 +45,11 @@ func main() {
 			firstNames = append(firstNames, names[0])
 		}
 		fmt.Printf("The first names of bookings are: %v\n", firstNames)
+
+		if remainingTickets == 0 {
+			//end program
+			fmt.Println("Our conference is booked out. Come back next year.")
+			break
+		}
 	}
 }
