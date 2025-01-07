3907aacb3c27233e39dd330c553bd9b333489a6b
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-01-06 17:37:22 -0700

    Learn arrays and slices

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ func main() {
 	var conferenceName = "Go conference"
 	const conferenceTickets = 50
 	var remainingTickets uint = 50
+	var bookings [50]string
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
@@ -31,6 +32,12 @@ func main() {
 	fmt.Scan(&userTickets)
 
 	remainingTickets = remainingTickets - userTickets
+	bookings[0] = firstName + " " + lastName
+
+	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("Array type: %T\n", bookings)
+	fmt.Printf("Array length: %v\n", len(bookings))
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
