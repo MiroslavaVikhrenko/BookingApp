6d89f3f6065689c83e31400d24d45ca7bac44a71
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-01-06 18:51:37 -0700

    Complete greetUsers() func

--- main.go
+++ main.go
@@ -11,11 +11,7 @@ func main() {
 	var remainingTickets uint = 50
 	bookings := []string{}
 
-	greetUsers()
-
-	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
-	fmt.Println("Get your tickets here to attend")
+	greetUsers(conferenceName, conferenceTickets, remainingTickets)
 
 	for {
 		var firstName string
@@ -72,6 +68,8 @@ func main() {
 	}
 }
 
-func greetUsers() {
-	fmt.Println("Welcome to our Conference")
+func greetUsers(confName string, confTickets int, remainingTickets uint) {
+	fmt.Printf("Welcome to %v booking application\n", confName)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", confTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
 }
